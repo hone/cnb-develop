2dbee305ed30685a70638211f1af6e7ccf4bef73
Terence Lee <hone02@gmail.com>
2020-05-29 19:10:27 -0500

    extra directories into constants

--- src/main.rs
+++ src/main.rs
@@ -5,12 +5,18 @@ use watchexec::run::{watch, ExecHandler};
 const DETECTOR_PATH: &str = "/cnb/lifecycle/detector";
 const BUILDER_PATH: &str = "/cnb/lifecycle/builder";
 const LAUNCHER_PATH: &str = "/cnb/lifecycle/launcher";
-const PROJECT_PATH: &str = "/project/";
+const PROJECT_PATH: &str = "/source/";
+const WORK_DIR_PATH: &str = "/workspace/";
 
 fn main() -> anyhow::Result<()> {
-    let rsync_to_cmd = "rsync -zrq --exclude .git --exclude node_modules /project/ /workspace/";
-    let rsync_from_cmd =
-        "rsync -zrq --include 'package-lock.json' --exclude '*' /workspace/ /project/";
+    let rsync_to_cmd = format!(
+        "rsync -zrq --exclude .git --exclude node_modules {} {}",
+        PROJECT_PATH, WORK_DIR_PATH
+    );
+    let rsync_from_cmd = format!(
+        "rsync -zrq --include 'package-lock.json' --exclude '*' {} {}",
+        WORK_DIR_PATH, PROJECT_PATH
+    );
 
     let args = ArgsBuilder::default()
         .cmd(vec![format!(
